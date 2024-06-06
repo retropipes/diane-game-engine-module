fd5eea139d5ed1a587d531ec48699d110cc1bd75
RetroPipes <eric.ahnell@puttysoftware.com>
2024-06-06 06:56:14 -0400

    Separate PasswordProtector.

--- diane-gui/src/module-info.java
+++ diane-gui/src/module-info.java
@@ -2,8 +2,6 @@ module org.retropipes.diane.gui {
 	requires transitive java.desktop;
 	requires transitive org.retropipes.diane.asset.image;
 	requires transitive org.retropipes.diane.asset.music;
-	requires transitive org.retropipes.diane.fileio;
-	requires transitive org.retropipes.diane.utility;
 	requires org.retropipes.diane.internal;
 
 	exports org.retropipes.diane.gui;
---
+++ diane-protect/src/module-info.java
@@ -0,0 +1,7 @@
+module org.retropipes.diane.protect {
+	requires transitive java.desktop;
+	requires transitive org.retropipes.diane.fileio;
+	requires transitive org.retropipes.diane.utility;
+	
+	exports org.retropipes.diane.protect;
+}
