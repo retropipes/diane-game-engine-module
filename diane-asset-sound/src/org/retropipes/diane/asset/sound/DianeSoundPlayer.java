a602180d0bebe0d720cab617f2db2278247d5ccf
RetroPipes <eric.ahnell@puttysoftware.com>
2025-12-24 17:24:42 -0500

    Diane Sound Player: Add Play Source support.

--- diane-asset-sound/src/org/retropipes/diane/asset/sound/DianeSoundPlayer.java
+++ diane-asset-sound/src/org/retropipes/diane/asset/sound/DianeSoundPlayer.java
@@ -3,12 +3,18 @@ Copyleft (C) 2019-present Eric Ahnell
 Any questions should be directed to the author via email at: support@puttysoftware.com */
 package org.retropipes.diane.asset.sound;
 
+import java.net.URL;
+
 public final class DianeSoundPlayer {
     // Constants
     protected static final int EXTERNAL_BUFFER_SIZE = 4096; // 4Kb
 
     public static void play(final DianeSoundIndex index) {
 	new SoundResource(index.getURL()).play();
+    }
+
+    public static void playSource(final URL source) {
+	new SoundResource(source).play();
     }
 
     // Constructor
