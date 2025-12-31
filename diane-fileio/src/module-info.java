3c42e98100bffff1c3c18bad2fbebb608252fa4d
RetroPipes <eric.ahnell@puttysoftware.com>
2025-12-31 09:12:47 -0500

    Add Legacy File I/O support.

--- diane-fileio/src/module-info.java
+++ diane-fileio/src/module-info.java
@@ -2,4 +2,5 @@ module org.retropipes.diane.fileio {
     requires transitive java.desktop;
 
     exports org.retropipes.diane.fileio;
+    exports org.retropipes.diane.fileio.legacy;
 }
---
+++ diane-fileio/src/org/retropipes/diane/fileio/legacy/UnexpectedTagException.java
@@ -0,0 +1,11 @@
+package org.retropipes.diane.fileio.legacy;
+
+import java.io.IOException;
+
+public class UnexpectedTagException extends IOException {
+    private static final long serialVersionUID = 23250505322336L;
+
+    public UnexpectedTagException(final String msg) {
+	super(msg);
+    }
+}
